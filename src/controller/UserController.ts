348ef0e82d6f9a3c988711f07ed2c0320edb929b
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2021-04-24 23:50:32 +0530

    Added support for user details with user info

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -8,19 +8,13 @@ import { ErrorDataType } from "../constant/ErrorData";
 import { postProcessPosts } from "../service/FeedService";
 import postgres from "../db/postgres";
 import { RsaKeyPostgres } from "../interfaces/database/RsaKeyPostgres";
+import { UserInfoResponse } from "../interfaces/responses/UserInfoResponse";
+import { FollowPostgres } from "../interfaces/database/FollowPostgres";
+import { fetchProfile } from "../service/UserProfileService";
 
 export const userProfile = async (
   req: Request<{ username: string }>,
-  res: Response<
-    | {
-        id: string;
-        username: string;
-        is_following: boolean;
-        is_approved: boolean;
-        public_key: string | null;
-      }
-    | ErrorDataType
-  >
+  res: Response<UserInfoResponse | ErrorDataType>
 ): Promise<void> => {
   const own_id = res.locals.user_id;
   const username = req.params.username;
@@ -29,7 +23,12 @@ export const userProfile = async (
     if (user == null) {
       res.status(404).send(errorResponse(404, "User not found"));
     } else {
-      const isFollowing = await checkFollow(own_id, user.id);
+      const isFollowing = await postgres<FollowPostgres>(
+        "group_follow_approvals"
+      )
+        .select("id", "is_approved", "followee_sym_key")
+        .where({ user_id: own_id, followee_id: user.id })
+        .first();
       let public_key: string | null = null;
       if (isFollowing == null) {
         const userRsaKey = await postgres<RsaKeyPostgres>("user_rsa_keys")
@@ -38,13 +37,21 @@ export const userProfile = async (
           .first();
         public_key = userRsaKey.public_key;
       }
-      res.status(200).send({
+      const response: UserInfoResponse = {
         id: user.id,
         username: user.username,
         is_following: isFollowing != null,
         is_approved: isFollowing != null && isFollowing.is_approved,
+        sym_key: isFollowing != null ? isFollowing.followee_sym_key : null,
         public_key: public_key,
-      });
+      };
+      const profile = await fetchProfile(user.id);
+      if (profile != null) {
+        response.first_name = profile.first_name;
+        response.last_name = profile.last_name;
+        response.profile_picture = profile.profile_picture;
+      }
+      res.status(200).send(response);
     }
   } catch (e) {
     console.error(e);
